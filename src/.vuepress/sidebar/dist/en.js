432bb8461d1596025a2184a6d3074bdde7950be4
hujiayucc <hujiayucc@qq.com>
2024-10-05 22:00:01 +0800

    Delete oid project files

--- src/.vuepress/sidebar/dist/en.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.enSidebar = void 0;
-var vuepress_theme_hope_1 = require("vuepress-theme-hope");
-exports.enSidebar = vuepress_theme_hope_1.sidebar({
-    "/": []
-});
--- src/.vuepress/sidebar/dist/zh.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.zhSidebar = void 0;
-var vuepress_theme_hope_1 = require("vuepress-theme-hope");
-exports.zhSidebar = vuepress_theme_hope_1.sidebar({
-    "/zh/": []
-});
